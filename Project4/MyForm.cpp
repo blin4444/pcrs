2555c4a87fcc0416b30c390429233c4f07288836
Billy Lin <b.lin@alumni.ubc.ca>
2013-03-16 01:03:41 -0700

    Nice buttons in MyForm

--- Project4/MyForm.cpp
+++ Project4/MyForm.cpp
@@ -1,6 +1,10 @@
 #include "MyForm.h"
 using namespace Project4;
 
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 int main()
 {
 	/*MyForm^ test = gcnew MyForm();
