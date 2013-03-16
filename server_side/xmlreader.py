ccb091b38766a466cbd3ae754d1d05d4ae6cb526
treitman <tedreitman@gmail.com>
2013-03-16 15:15:04 -0700

    Update xmlreader.py

--- server_side/xmlreader.py
+++ server_side/xmlreader.py
@@ -9,6 +9,7 @@ class XMLParser:
         list = root.findall("./*")
         for section in list:
             self.loadSection(section)
+        self.buildClientForm(list)
 
     def loadSection(self, section):
         list = section.findall(".")
@@ -18,26 +19,37 @@ class XMLParser:
             self.buildQuery(element)
 
     def buildQuery(self, element):
-      columns = element.findall("./*")
-    	table = element.attrib["table"]
-    	string = MutableString()
-    	string += "insert into "
-    	string2 = MutableString()
-    	string2 += "values("
-    	string += table + "("
-    	for i in range(len(columns)-1):
-    		string += columns[i].attrib['column'] + ", "
-    		if columns[i].attrib['type'] != "integer":
-    			string2 += "'%s', "
-    		else:
-    			string2 += "%s, "
-    	string += columns[len(columns)-1].attrib['column'] + ") "
-    	if columns[len(columns)-1].attrib['type'] != "integer":
-    		string2 += "'%s') "
-    	else:
-    		string2 += "%s)"
-    	string += string2
-    	print string
+        columns = element.findall("./*")
+        table = element.attrib["table"]
+        string = MutableString()
+        string += "insert into "
+        string2 = MutableString()
+        string2 += "values("
+        string += table + "("
+        for i in range(len(columns)-1):
+            string += columns[i].attrib['column'] + ", "
+            if columns[i].attrib['type'] != "integer":
+                string2 += "'%s', "
+            else:
+                string2 += "%s, "
+        string += columns[len(columns)-1].attrib['column'] + ") "
+        if columns[len(columns)-1].attrib['type'] != "integer":
+            string2 += "'%s') "
+        else:
+            string2 += "%s)"
+        string += string2
+        print string
+        return string
+
+    def buildClientForm(self, list):
+        string = MutableString()
+        for i in range(len(list)-1):
+            sections = list[i].findall("./*")
+            for i in range(len(sections)-1):
+                string += sections[i].attrib["id"] + "," + sections[i].attrib["type"] + ";"
+                string += sections[i].attrib["id"] + "," + sections[i].attrib["type"]
+        print string
+        return string
 
 if __name__ == "__main__":
     xml = XMLParser()
