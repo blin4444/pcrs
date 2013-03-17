386c404e9f2464fc3fcc47aee178b7eb6d079c08
MattGingerich <gingerich.matthew@gmail.com>
2013-03-17 06:03:25 -0700

    Reception (user info) communication.

--- Receptionist/ServerCommunication.cpp
+++ Receptionist/ServerCommunication.cpp
@@ -1,6 +1,47 @@
 #include "ServerCommunication.h"
 
-
 ServerCommunication::ServerCommunication(void)
 {
+	client = gcnew HttpClient();
+}
+
+int ServerCommunication::sendRequest(String^ host, String^ path, Collections::Generic::List<KeyValue^>^ args, String^ result)
+{
+	int resultCode = 0;
+	HttpRequestMessage^ message = gcnew HttpRequestMessage();
+	System::Uri^ server_uri = gcnew System::Uri("http://" + host + "/" + path + "/");
+	StringContent^ content = gcnew System::Net::Http::StringContent("");
+	for each (KeyValue^ arg in args)
+	{
+		message->Headers->Add(arg->key, arg->value);
+	}
+	message->RequestUri = server_uri;
+	message->Content = content;
+	message->Method = HttpMethod::Post;
+	auto task = client->SendAsync(message);
+
+	task->Wait();
+	HttpResponseMessage^ response = task->Result;
+	HttpContent^ resultContent = response->Content;
+	result = resultContent->ReadAsStringAsync()->Result;
+	resultCode = Convert::ToInt32(result[0])-48;
+
+	return resultCode;
+}
+
+String^ ServerCommunication::GetXmlFile(System::String^ file)
+{
+	Collections::Generic::List<KeyValue^>^ args = gcnew Collections::Generic::List<KeyValue^>();
+	args->Add(gcnew KeyValue("file", file));
+	String^ result;
+	if (!sendRequest("localhost:9999", "getxml", args, result))
+		return result;
+	else
+		return nullptr;
+}
+
+int ServerCommunication::SubmitUserInfo(Collections::Generic::List<KeyValue^>^ args)
+{
+	String^ result;
+	return sendRequest("localhost:9999", "getxml", args, result);
 }
--- Receptionist/ServerCommunication.h
+++ Receptionist/ServerCommunication.h
@@ -1,8 +1,26 @@
 #pragma once
+
+using namespace System;
+using namespace System::Net::Http;
+
+ref class KeyValue {
+	public:
+		KeyValue(String^ key, String^ value)
+		{
+			this->key = key;
+			this->value = value;
+		}
+		String^ key;
+		String^ value;
+};
+
 ref class ServerCommunication
 {
 public:
 	ServerCommunication(void);
-
+	String^ GetXmlFile(System::String^ file);
+	int SubmitUserInfo(Collections::Generic::List<KeyValue^>^ args);
+	int sendRequest(String^ host, String^ path, Collections::Generic::List<KeyValue^>^ args, String^ result);
+private:
+	HttpClient^ client;
 };
-
