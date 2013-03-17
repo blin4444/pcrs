6155c4ad93401c9237d3c752feb2baa3ea430cd3
treitman <tedreitman@gmail.com>
2013-03-17 07:34:29 -0700

    Update xmlgen.py

--- server_side/xmlgen.py
+++ server_side/xmlgen.py
@@ -10,7 +10,7 @@ def options_xml(options_list):
 			if prev_title != item[0]:
 				if prev_title != "":
 					output.write('\t</div>\n')
-				output.write('\t<div name="'+item[0]+'">\n')
+				output.write('\t<div id="'+item[0]+'">\n')
 				prev_title = item[0]
 			output.write('\t\t<input id="'+str(item[1])+'" value="'+item[2]+'" type="radio"/>\n')
 		output.write('\t</div>\n');
