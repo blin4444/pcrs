0ba64ecc88177be0c6ca75f8f3e50c53487f54d1
treitman <tedreitman@gmail.com>
2013-03-16 21:20:09 -0700

    Update form.py

--- server_side/form.py
+++ server_side/form.py
@@ -15,12 +15,12 @@ class Form:
         return ET.fromstring(string)
 
     def printSections(self):
-        sections = self.root.findall("./*")
+        sections = self.root.findall("./div")
         for section in sections:
             self.buildQuery(section)
 
     def buildQuery(self, section):
-        elements = section.findall("./*")
+        elements = section.findall("./input")
         table = section.attrib["id"]
         string = MutableString()
         string += "insert into "
@@ -28,39 +28,22 @@ class Form:
         string2 += "values("
         string += table + "("
         for i in range(len(elements)-1):
-            string += elements[i].attrib['column'] + ", "
+            string += elements[i].attrib['id'] + ", "
             string2 += "%s, "
-        string += columns[len(columns)-1].attrib['column'] + ") "
+        string += elements[len(elements)-1].attrib['id'] + ") "
         string2 += "'%s') "
         string += string2
         print string
         return str(string)
 
-    def buildClientForm(self):
-        sections = self.root.findall("./*")
-        string = MutableString()
-        for i in range(len(sections)-1):
-            fields = sections[i].findall("./*")
-            for i in range(len(fields)-1):
-                string += fields[i].attrib["id"] + "," + fields[i].attrib["type"] + ";"
-                string += fields[i].attrib["id"] + "," + fields[i].attrib["type"]
-        print string
-        return string
-
     def buildArgumentMap(self):
-        sections = self.root.findall("./*")
+        sections = self.root.findall("./div")
         mapping = dict()
         for section in sections:
             arguments = list()
             mapping[section.attrib["id"]] = arguments
-            fields = section.findall("./*")
+            fields = section.findall("./input")
             for field in fields:
                 arguments.append(field.attrib["id"])
         print mapping
         return mapping
-
-if __name__ == "__main__":
-    xml = Form("client_information_form.xml")
-    xml.printSections()
-    xml.buildClientForm()
-    xml.buildArgumentMap()
